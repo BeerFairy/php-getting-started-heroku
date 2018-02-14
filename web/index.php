d0e22bc817cec2b4d5e248b1189d3a91ae0988e6
BeerFairy <36173682+BeerFairy@users.noreply.github.com>
2018-02-14 20:20:13 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -11,6 +11,11 @@ Hello
   <?php if (isset($_GET['submitted'])) { ?>
     <h1>The server had accepted:</h1>
     <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
+    <p>checkbox: <?php echo htmlspecialchars($_GET['checkbox']); ?></p>
+  <p>select: <?php echo htmlspecialchars($_GET['select']); ?></p>
+  <p>color: <?php echo htmlspecialchars($_GET['color']); ?></p>
+  <p>date: <?php echo htmlspecialchars($_GET['date']); ?></p>
+  <p>month: <?php echo htmlspecialchars($_GET['month']); ?></p>
     <p>Try again? <a href="/index.php">Yes!</a></p>
   <?php } ?>
 
@@ -18,7 +23,8 @@ Hello
   
   <h1>Play with the HTML form</h1>
   <form action="/index.php">
-    <input type="hidden" name="submitted" value="yes">
+    <div style="border: 2px solid red;">
+      <input type="hidden" name="submitted" value="yes">
     <p>
       <label for="my-radio-yes">radio Yes: </label>
       <input id="my-radio-yes" name="radio" type="radio" required value="Yes">
@@ -27,6 +33,7 @@ Hello
     <br>
     <input type="submit" value="Submit">
     <input type="reset" value="Clear">
+    </div>
     <p><label for="my-checkbox">checkbox:</label>
       <input id="my-checkbox" name="my-checkbox" type="checkbox" checked></p>
     <p><label for ="my-select">select: </label><select id="my-select" name="my-select" size="1" required>
@@ -39,8 +46,10 @@ Hello
     <p><label for="my-date">Дата: </label><input id="my-date" name="my-date" type="date" required></p>
     <p><label for="my-month" >Месяц: </label><input id="my-month" name="my-month" type="month" required></p>
   </form>
+  </div>
 
   <?php } ?>
 
+  
 </body>
 </html>
